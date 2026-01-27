0959c70737008e9103a504b4a2b3161add1371c6
Tushar Khatri <hello@tusharkhatri.in>
2026-01-27 12:51:27 +0530

    fix: rule controllers

--- src/main/java/com/eventara/rule/controller/RuleTestController.java
+++ src/main/java/com/eventara/rule/controller/RuleTestController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/rules/test")
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin(origins = "*")
 public class RuleTestController {
 
     private final RuleService ruleService;
